1bc3f6160b6894b27c583b581aec33d9feeb09f7
joeyhanni2 <joeyhanni@gmail.com>
2023-02-19 01:32:13 +0300

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main()
     printf("Size of a long int: %d byte(s)\n",sizeof(long int));
     printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
     printf("Size of a float: %d byte(s)\n",sizeof(float));
-    return 0;
+    return (0); 
 }
 
