e8aa8a1aaaca3eb5eea78ef3116ab8e53ae4dd06
joeyhanni2 <joeyhanni@gmail.com>
2023-06-27 16:24:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * _strlen: returns the length of a string
- * @str: pointer to character array
- * Returns: length of the string
+ * _strlen - returns the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
  */
 
-size_t _strlen(const char *str)
+size_t _strlen(char *str)
 {
-size_t length = 0;
+size_t lenStr = 0;
 
 while (*str++)
-return (length);
+lenStr++;
+
+return (lenStr);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string to print.
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
