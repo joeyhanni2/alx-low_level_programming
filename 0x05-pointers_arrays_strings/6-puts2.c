b3a5d21ed21bf3d14f90fb17e3013286463562be
joeyhanni2 <joeyhanni@gmail.com>
2023-06-27 18:16:10 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,14 @@ void puts2(char *str)
 {
 int i;
 int j = 0;
-
-while (str[i] != '\0')
+while (str[j] != '\0')
 {
-if (i % 2 == 0)
+j++;
+}
+
+for (i = 0; i < j; i += 2)
+{
 _putchar(str[i]);
-i++;
 }
 _putchar('\n');
 }
