4668d21961aa7e20835fa645aa51cc4969324982
joeyhanni2 <joeyhanni@gmail.com>
2023-06-30 13:16:45 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,19 +5,19 @@
  * @dest: pointer to destination
  * @src: pointer to source
  * @n: number of bytes
- * Return:void
+ * Return: void
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, dest_len = 0;
+int dest_len, i;
 
 for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 ;
 for (i = 0; i < n && src[i] != '\0'; i++)
-dest[dest_len++] = src[i];
+dest[dest_len + i] = src[i];
 
-dest[i] = '\0';
+dest[dest_len + i] = '\0';
 
 return (dest);
 }
