16310dd77aaeaa6bd9aa7d38916999d31bfe64f8
joeyhanni2 <joeyhanni@gmail.com>
2023-06-30 17:03:59 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-i = 0
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*s1 && (*s1 == *s2))
 {
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
+s1++;
+s2++;
 }
-i++;
-}
-return (0);
+return (s1 - s2);
 }
